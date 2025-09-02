2ec53aa4a029d56fb4ec52d1997bd09a73e4ee65
talha-dev <talha191999@gmail.com>
2025-09-02 15:52:11 +0500

    feat: setup ESlint and prettier also installed SASS

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,6 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { FlatCompat } from '@eslint/eslintrc';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -10,15 +10,20 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends('next/core-web-vitals', 'next/typescript'),
   {
-    ignores: [
-      "node_modules/**",
-      ".next/**",
-      "out/**",
-      "build/**",
-      "next-env.d.ts",
-    ],
+    ignores: ['node_modules/**', '.next/**', 'out/**', 'build/**', 'next-env.d.ts'],
+  },
+  {
+    plugins: {
+      prettier: require('eslint-plugin-prettier'),
+      'unused-imports': require('eslint-plugin-unused-imports'),
+    },
+    rules: {
+      'prettier/prettier': 'error',
+      'unused-imports/no-unused-imports': 'warn',
+      'react/react-in-jsx-scope': 'off',
+    },
   },
 ];
 
